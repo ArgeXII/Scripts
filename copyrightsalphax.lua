58dae388834b6c1d53f985aa2aa0f77cf6d6a893
ArgeXII <159491068+ArgeXII@users.noreply.github.com>
2024-02-09 17:55:49 -0500

    Update copyrightsalphax.lua

--- copyrightsalphax.lua
+++ copyrightsalphax.lua
@@ -150,7 +150,8 @@ local mobPositions = {
     ["Frozen Rogue Experiments"] = Vector3.new(-11963.912109375, 434.88238525390625, 10376.19140625),
     ["Zuto"] = Vector3.new(-13131.482421875, 569.38818359375, 8901.443359375),
     ["Turtles"] = Vector3.new(-2732.229736328125, 1466.5418701171875, -6181.73779296875),
-    ["Sell Tier 1 & 2"] = Vector3.new(-6014.44580078125, 2333.727294921875, 8712.095703125),
+    ["Sell Tier 1"] = Vector3.new(-6074.64697265625, 2335.641845703125, 8764.91015625),
+    ["Sell Tier 2"] = Vector3.new(-6074.64697265625, 2335.641845703125, 8764.91015625),
     ["Skull Android"] = Vector3.new(-10187.318359375, 582.6741333007812, 3835.489990234375),
     ["Nappa"] = Vector3.new(-10187.318359375, 582.6741333007812, 3835.489990234375),
     ["Elf"] = Vector3.new(-20694.3203125, 483.656005859375, 554.5396118164062),
@@ -448,7 +449,7 @@ end)
 
 --] Level Dropdowns [--
 
-local Dropdown_Mob = Level:CreateDropdown("Select a Mob", {"nil", "Zuto", "Frozen Rogue Experiments", "Snowkid", "Sell Tier 1 & 2", "Skull", "Azura", "Pikkon", "Turtles", "Nappa", "Cyber", "Gero", "Yeti King", "Android 13", "Nash Ketchup", "Ape", "Snowman", "Elf", "Santa", "Frost SSJ4", "SSJ4", "Frost", "Thief", "Thief Boss", "Bear Minion", "Bear King", "Aien", "Rogue Experiments", "Green Saibaman", "Red Saibaman", "Mountain Fighter", "Monster Saibablue", "Evil Namekian", "Martial Artists", "Spopov", "Yam", "Farmer", "Mobster", "Mob Mobster", "Elite Alien", "Funny Guy", "Saibablue", "Mutant Saibaman", "Evil Saiyan", "Postboy Namekian", "Greater Spopov", "Greater Yam", "Evil Majin", "Evil Saiyan", "Prototype Android", "Corrupted Kai", "Robert", "Boku Black", "Desert Bandit", "Ajax Follower" }, "nil", 0.25, function(newmobxd)
+local Dropdown_Mob = Level:CreateDropdown("Select a Mob", {"nil", "Zuto", "Frozen Rogue Experiments", "Snowkid", "Sell Tier 1", "Sell Tier 2", "Skull", "Azura", "Pikkon", "Turtles", "Nappa", "Cyber", "Gero", "Yeti King", "Android 13", "Nash Ketchup", "Ape", "Snowman", "Elf", "Santa", "Frost SSJ4", "SSJ4", "Frost", "Thief", "Thief Boss", "Bear Minion", "Bear King", "Aien", "Rogue Experiments", "Green Saibaman", "Red Saibaman", "Mountain Fighter", "Monster Saibablue", "Evil Namekian", "Martial Artists", "Spopov", "Yam", "Farmer", "Mobster", "Mob Mobster", "Elite Alien", "Funny Guy", "Saibablue", "Mutant Saibaman", "Evil Saiyan", "Postboy Namekian", "Greater Spopov", "Greater Yam", "Evil Majin", "Evil Saiyan", "Prototype Android", "Corrupted Kai", "Robert", "Boku Black", "Desert Bandit", "Ajax Follower" }, "nil", 0.25, function(newmobxd)
     mob = newmobxd
 end)
 
